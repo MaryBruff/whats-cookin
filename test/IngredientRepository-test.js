3988e31936ab98efe597962dfd43957620319a3a
Jen Nguyen <jenniferannnguyen@live.com>
2023-09-13 22:25:15 -0600

    Add start of beforeEach hook needed

--- test/IngredientRepository-test.js
+++ test/IngredientRepository-test.js
@@ -8,12 +8,17 @@ const {
 } = require("../src/recipes");
 
 describe("Get Ingredients", () => {
-  //happy path
+  let recipe;
+  let ingredients; 
+
+  beforeEach(() => {
+    recipe = recipeTestData[0]
+    ingredients = ingredientsTestData
+  });
+  
   it("Should determine the list of ingredients for a recipe", () => {
     const ingredientList = getIngredientNames(
-      recipeTestData[0],
-      ingredientsTestData
-    );
+      recipe, ingredients);
     expect(ingredientList).to.deep.equal([
       "wheat flour",
       "bicarbonate of soda",
@@ -29,7 +34,18 @@ describe("Get Ingredients", () => {
     ]);
   });
   
-  it("Should handle recipe with multiple identical ingredients", () => {
+  it("Should handle recipe ingredients not present in the provided ingredients list", () => {
+    const recipe = {
+      ingredients: [
+        { id: 10, name: "Unknown Ingredient" },
+        { id: 20, name: "Another Unknown Ingredient" },
+      ],
+    };
+    const ingredientNames = getIngredientNames(recipe, ingredients);
+    expect(ingredientNames).to.deep.equal([]);
+  });
+  
+ it("Should handle recipe with multiple identical ingredients", () => {
     const recipe = {
       ingredients: [
         { id: 1, name: "Flour" },
@@ -43,39 +59,24 @@ describe("Get Ingredients", () => {
     ];
     const ingredientNames = getIngredientNames(recipe, ingredients);
     expect(ingredientNames).to.deep.equal(["Flour", "Sugar"]);
-  });
-  
-  //sad paths
-  it("Should return an empty array when recipe has no ingredients", () => {
-    const recipe = {
-      ingredients: [],
-    };
-    const ingredients = [
-      { id: 1, name: "Flour" },
-      { id: 2, name: "Sugar" },
-    ];
-    const ingredientNames = getIngredientNames(recipe, ingredients);
-    expect(ingredientNames).to.deep.equal([]);
-  });
-  
-  it("Should handle recipe ingredients not present in the provided ingredients list", () => {
-    const recipe = {
-      ingredients: [
-        { id: 10, name: "Unknown Ingredient" },
-        { id: 20, name: "Another Unknown Ingredient" },
-      ],
-    };
-    const ingredients = [
-      { id: 1, name: "Flour" },
-      { id: 2, name: "Sugar" },
-    ];
-    const ingredientNames = getIngredientNames(recipe, ingredients);
-    expect(ingredientNames).to.deep.equal([]);
+    
+    it("should throw an error if ingredient id does not exist", () => {
+      const recipe = {
+        id: 2,
+        ingredients: [{ id: 2, quantity: { amount: 1 } }],
+      };
+      const ingredients = [{ id: 1, estimatedCostInCents: 100 }];
+      try {
+        calculateCost(recipe, ingredients);
+        expect.fail("Expected an error to be thrown");
+      } catch (error) {
+        expect(error.message).to.equal("Ingredient not found");
+      }
+    });
   });
 });
 
 describe("calculateCost", () => {
-  //happy paths
   it("should calculate the cost of a recipe with one ingredient", () => {
     const recipe = {
       id: 1,
@@ -120,18 +121,4 @@ describe("calculateCost", () => {
     expect(totalCost).to.equal(expectedTotalCost);
   });
   
-  //sad path
-  it("should throw an error if ingredient id does not exist", () => {
-    const recipe = {
-      id: 2,
-      ingredients: [{ id: 2, quantity: { amount: 1 } }],
-    };
-    const ingredients = [{ id: 1, estimatedCostInCents: 100 }];
-    try {
-      calculateCost(recipe, ingredients);
-      expect.fail("Expected an error to be thrown");
-    } catch (error) {
-      expect(error.message).to.equal("Ingredient not found");
-    }
-  });
 });
